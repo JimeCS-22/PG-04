732bbdca206b978e9de10ba5873a858a4f76b31f
Lab01 <alexanderleonmachado35@gmail.com>
2026-05-06 20:24:06 -0600

    ArrayQueue

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module ucr.algoritmos.pg04algoritmos {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.base;
+    requires java.desktop;
 
 
     opens ucr.algoritmos.pg04algoritmos to javafx.fxml;
--- src/main/java/ucr/algoritmos/pg04algoritmos/model/Queue/ArrayQueue.java
+++ src/main/java/ucr/algoritmos/pg04algoritmos/model/Queue/ArrayQueue.java
@@ -1,11 +1,13 @@
 package ucr.algoritmos.pg04algoritmos.model.Queue;
 
+import javax.swing.text.html.HTMLDocument;
+
 public class ArrayQueue<T> implements MyQueue<T> {
     private int n; //el tam max de la cola
     private T[] data; //arreglo de objetos
     private Integer[] priorityQueue; //para el manejo de prioridades
-    //me permite manejar los extremos de la cola
-    private int front, rear; //anterior, posterior
+    private int front, rear; //anterior, posterior, me permite manejar los extremos de la cola
+
     //Constructor
     public ArrayQueue(int n){
         if(n<=0) System.exit(1); //se sale
@@ -16,19 +18,15 @@ public class ArrayQueue<T> implements MyQueue<T> {
         this.front = rear;
     }
 
-    private boolean isFull() {
-        return (rear + 1) % n == front;
-    }
     @Override
     public int size() {
         // cantidad de elementos en cola circular
-        return (rear - front + n) % n;
+        return rear - front;
     }
 
     @Override
     public void clear() {
         this.data = (T[]) new Object[n];
-        this.priorityQueue = new Integer[n];
         this.rear = n-1; //ultimo elemento de la cola
         this.front = rear;
     }
@@ -38,6 +36,7 @@ public class ArrayQueue<T> implements MyQueue<T> {
         return front == rear;
     }
 
+    //Revisar
     @Override
     public int indexOf(T element) throws QueueException {
         if(isEmpty()) throw new QueueException("Array Queue is empty");
@@ -52,32 +51,31 @@ public class ArrayQueue<T> implements MyQueue<T> {
         return -1; // no encontrado
     }
 
+
     @Override
     public void enQueue(T element) throws QueueException {
-        if(isFull()) throw new QueueException("Array Queue is full");
+        if(size() == data.length) throw new QueueException("Array Queue is full");
+        //La primera vez cuando la cola está vacía, no entra al for
+        for (int i = front; i < rear; i++) {
+            data[i] = data[i+1]; // Mueve el elemento una posición a la izquierda
+        }
+        //Siempre encola por el extremo posterior
         data[rear] = element;
-        rear = (rear + 1) % n;
-        priorityQueue[rear] = null; //Sin prioridad
+        front--; //La idea es que anterior quede en un campo vacío
     }
 
+    //Revisar
     @Override
     public T deQueue() throws QueueException {
-        if (isEmpty())
-            throw new QueueException("Array Queue is empty");
-
-        front = (front + 1) % n;
-        T removed = data[front];
-        data[front] = null;
-        priorityQueue[front] = null;
-        return removed;
+        if (isEmpty()) throw new QueueException("Array Queue is empty");
+        return data[++front];
     }
 
+    //Revisar
     @Override
     public void enQueue(T element, Integer priority) throws QueueException {
-        if (priority == null)
-            throw new QueueException("Priority cannot be null");
-        if (isFull())
-            throw new QueueException("Array Queue is full");
+        if (priority == null) throw new QueueException("Priority cannot be null");
+        if (size() == data.length) throw new QueueException("Array Queue is full");
 
         // Si está vacía, encola directo
         if (isEmpty()) {
@@ -140,13 +138,37 @@ public class ArrayQueue<T> implements MyQueue<T> {
 
     @Override
     public T peek() throws QueueException {
-        return front();
+        if (isEmpty()) throw new QueueException("Array Queue is empty");
+        return data[front+1];
     }
 
     @Override
     public T front() throws QueueException {
-        if (isEmpty())
-            throw new QueueException("Array Queue is empty");
-        return data[(front + 1) % n];
+        if (isEmpty()) throw new QueueException("Array Queue is empty");
+        return data[front + 1];
+    }
+
+    @Override
+    public String toString(){
+        if(isEmpty()) return "Array Queue is empty";
+        StringBuilder sb = new StringBuilder("FRONT ➡️ ");
+        ArrayQueue<T> auxQueue = new ArrayQueue<>(size());
+        try {
+        while(!isEmpty()) {
+            sb.append("[").append(peek()).append("] ");
+            auxQueue.enQueue(deQueue());
+            if (!isEmpty()) sb.append(", ");
+        }
+
+        //Al final dejamos la cola en su estado original
+        while(!auxQueue.isEmpty()) {
+            enQueue(auxQueue.deQueue());
+        }
+
+        } catch (QueueException e) {
+            throw new RuntimeException(e);
+        }
+        sb.append(" ➡️ REAR");
+        return sb.toString();
     }
 }
